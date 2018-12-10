421b2418cb89db584d43d8581175dbe7800a2ec5
ViacheslavZuberu <clavik15@gmail.com>
2018-12-10 23:54:29 +0200

    Fixed key generating

--- server.js
+++ server.js
@@ -5,40 +5,55 @@ const bodyPaeser = require("body-parser");
 const NodeRSA = require("node-rsa");
 
 var app = express();
-var key = new NodeRSA({ b: 512 });
+var key = new NodeRSA({ b: 1024 });
 
 app.use(bodyPaeser.json());
 app.use(bodyPaeser.urlencoded({ extended: true }));
 
-var public = key.exportKey("components-public");
+var publicKey = key.exportKey("pkcs8-public-pem");
 
 app.get("/", function(req, res) {
-  res.send("Data Protection Lab #2");
+    res.send("Data Protection Lab #2 v0.5");
+});
+
+app.get("/regen_key", function(req, res) {
+    try {
+        key = new NodeRSA({ b: 1024 });
+        publicKey = key.exportKey("pkcs8-public-pem");
+    } catch (err) {
+        res.send({
+            result: "Error",
+            error: err
+        });
+    }
+    res.send({
+        result: "OK"
+    });
 });
 
 app.get("/public_key", function(req, res) {
-  res.send({
-    public_key: public
-  });
+    res.send({
+        public_key: publicKey
+    });
 });
 
 app.get("/test", function(req, res) {
-  var message = req.body.message;
-  res.send({
-    encrypted_message: key.encrypt(message, "base64")
-  });
+    var message = req.body.message;
+    res.send({
+        encrypted_message: key.encrypt(message, "base64")
+    });
 });
 
 app.post("/message", function(req, res) {
-  var message = key.decrypt(req.body.encrypted_message, "utf8");
-  res.send({
-    message_count_letter: message.length,
-    message_count_words: message.split(" ").length,
-    message_decrypted: message,
-    server_time: new Date().toISOString()
-  });
+    var message = key.decrypt(req.body.encrypted_message, "utf8");
+    res.send({
+        message_count_letter: message.length,
+        message_count_words: message.split(" ").length,
+        message_decrypted: message,
+        server_time: new Date().toISOString()
+    });
 });
 
 app.listen(PORT, function() {
-  console.log("API is started on port " + PORT);
+    console.log("API is started on port " + PORT);
 });
