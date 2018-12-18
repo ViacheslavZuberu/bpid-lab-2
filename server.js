912b0ea0cedce016407b49d7ddf76166edccc44b
ViacheslavZuberu <clavik15@gmail.com>
2018-12-18 23:42:04 +0200

    Changed the project

--- server.js
+++ server.js
@@ -1,68 +1,42 @@
-var PORT = process.env.PORT || 5000;
-
 const express = require("express");
 const bodyPaeser = require("body-parser");
-const NodeRSA = require("node-rsa");
+const random = require("random-number");
 
-var app = express();
-var key = new NodeRSA({ b: 1024 });
+const PORT = process.env.PORT || 5000;
 
-var data = [];
+let randomOptions = {
+    min: 25,
+    max: 500,
+    integer: true
+};
+
+let localDatabase = {
+    g: random(randomOptions),
+    p: random(randomOptions),
+    users: [],
+    messages: [],
+    archived_messages: []
+};
+
+app = express();
 
 app.use(bodyPaeser.json());
 app.use(bodyPaeser.urlencoded({ extended: true }));
 
-var publicKey = key.exportKey("pkcs8-public-pem");
-
-app.get("/", function(req, res) {
-    var result = "";
-
-    for (var i = data.length - 1; i >= 0; i--) {
-        result += data[i] + "<br>";
-    }
-
-    res.send("Data Protection Lab #2 v0.5<br>" + result);
-});
-
-app.get("/regen_key", function(req, res) {
-    try {
-        key = new NodeRSA({ b: 1024 });
-        publicKey = key.exportKey("pkcs8-public-pem");
-    } catch (err) {
-        res.send({
-            result: "Error",
-            error: err
-        });
-    }
-    res.send({
-        result: "OK"
-    });
-});
-
-app.get("/public_key", function(req, res) {
-    res.send({
-        public_key: publicKey
-    });
-});
-
-app.get("/test", function(req, res) {
-    var message = req.body.message;
-    res.send({
-        encrypted_message: key.encrypt(message, "base64")
-    });
-});
-
-app.post("/message", function(req, res) {
-    var message = key.decrypt(req.body.encrypted_message, "utf8");
-    data.push(new Date().toISOString() + " " + message);
-    res.send({
-        message_count_letter: message.length,
-        message_count_words: message.split(" ").length,
-        message_decrypted: message,
-        server_time: new Date().toISOString()
+app.post("/reg", (req, res, next) => {
+    let user = {
+        username: req.body.username,
+        number: random(randomOptions)
+    };
+    localDatabase.users.push(user);
+    res.status(201).json({
+        my_number: user.number,
+        g: localDatabase.g,
+        p: localDatabase.p
     });
 });
 
 app.listen(PORT, function() {
     console.log("API is started on port " + PORT);
+    console.info("Local Database: ", localDatabase);
 });
